ba73e74293f6f7101393a96d2b9f09b8d64c4de5
Jordan Graham <jordan@handsomedogstudio.com>
2017-04-17 16:46:57 -0600

    Updated scrub.sql and project root .gitignore.

--- mysql-dump/scrub.sql
+++ mysql-dump/scrub.sql
@@ -1,4 +1,4 @@
 TRUNCATE TABLE cache;
 UPDATE system SET STATUS = 1 WHERE name = 'stage_file_proxy';
 REPLACE INTO variable SET name = 'stage_file_proxy_hotlink', value = 'i:1;';
-REPLACE INTO variable SET name = 'stage_file_proxy_origin', value = 's:23:"http://jordangraham.com";';
+REPLACE INTO variable SET name = 'stage_file_proxy_origin', value = 's:28:"http://handsomedogstudio.com";';
